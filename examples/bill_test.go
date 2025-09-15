519a5a2f7ba8803c4a9b0c994d4eb9117a39ea87
Joseph Chen <jchen42703@gmail.com>
2025-09-14 22:30:01 -0400

    feat(examples): create bill with attachment example

---
+++ examples/bill_test.go
@@ -0,0 +1,106 @@
+package examples
+
+import (
+	"bytes"
+	"encoding/json"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/rwestlund/quickbooks-go/v2"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateBill(t *testing.T) {
+	// If just testing, get values from:
+	// https://developer.intuit.com/app/developer/playground
+	clientId := "<your-client-id>"
+	clientSecret := "<your-client-secret>"
+	realmId := "<realm-id>"
+
+	token := quickbooks.BearerToken{
+		RefreshToken: "<saved-refresh-token>",
+		AccessToken:  "<saved-access-token>",
+	}
+	qbClient, err := quickbooks.NewClient(clientId, clientSecret, realmId, false, "", &token)
+	require.NoError(t, err)
+
+	accs, err := qbClient.FindAccounts()
+	require.NoError(t, err)
+
+	var accPayableId string
+	var accExpenseId string
+	for _, acc := range accs {
+		if acc.Name == "Accounts Payable (A/P)" {
+			accPayableId = acc.Id
+		}
+
+		if acc.AccountType == quickbooks.CostOfGoodsSoldAccountType {
+			accExpenseId = acc.Id
+		}
+	}
+	require.NotEmpty(t, accPayableId)
+	require.NotEmpty(t, accExpenseId)
+
+	vendors, err := qbClient.FindVendors()
+	require.NoError(t, err)
+
+	var vendorId string
+	for _, vendor := range vendors {
+		if vendor.CompanyName == "Norton Lumber and Building Materials" {
+			vendorId = vendor.Id
+		}
+	}
+	require.NotEmpty(t, vendorId)
+
+	invoiceDate := time.Date(2025, time.June, 2, 0, 0, 0, 0, time.UTC)
+	bill, err := qbClient.CreateBill(&quickbooks.Bill{
+		TxnDate: quickbooks.Date{Time: invoiceDate},
+		VendorRef: quickbooks.ReferenceType{
+			Value: vendorId,
+		},
+		APAccountRef: quickbooks.ReferenceType{
+			Value: accPayableId,
+		},
+		DocNumber: "MUN-1000",
+		TotalAmt:  json.Number("103.55"),
+		CurrencyRef: quickbooks.ReferenceType{
+			Name:  "United States Dollar",
+			Value: "USD",
+		},
+		Line: []quickbooks.Line{
+			{
+				DetailType:  "AccountBasedExpenseLineDetail",
+				Description: "Lumber",
+				Amount:      json.Number("103.55"),
+				AccountBasedExpenseLineDetail: quickbooks.AccountBasedExpenseLineDetail{
+					AccountRef: quickbooks.ReferenceType{
+						Value: accExpenseId,
+					},
+				},
+			},
+		},
+	})
+	require.NoError(t, err)
+	t.Logf("bill: %+v", bill)
+
+	// Create the attachment
+	rawData, err := os.ReadFile("test.png")
+	require.NoError(t, err)
+	attachable, err := qbClient.UploadAttachable(&quickbooks.Attachable{
+		FileName:    "test.png",
+		ContentType: quickbooks.PNG,
+		AttachableRef: []quickbooks.AttachableRef{
+			{
+				EntityRef: quickbooks.ReferenceType{
+					Type:  "Bill",
+					Value: bill.Id,
+				},
+			},
+		},
+	}, bytes.NewReader(rawData))
+	require.NoError(t, err)
+	t.Logf("attachable: %+v", attachable)
+
+	// TODO: need to support creating the bill payment
+}
